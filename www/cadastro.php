ca7a1a715976a2dd119ffabc4bd727b2892ebae9
EstruttiJP <716cobre@gmail.com>
2024-12-19 17:31:53 -0300

    :bricks: ci: Linhas comentadas e Modificação no Docker-compose

--- www/cadastro.php
+++ www/cadastro.php
@@ -12,7 +12,7 @@
     <meta name="keywords" content="" />
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body
@@ -21,30 +21,37 @@
 
     <div class="bg-white p-8 rounded-lg shadow-md w-96 bg-opacity-30 backdrop-blur-md">
         <h2 class="text-3xl font-bold mb-6 text-center">Cadastro</h2>
+
+        <!-- Formulário para cadastro de conta nova -->
         <form action="#">
             <div class="mb-4">
+                <!-- Input de nome de usuario, name = name -->
                 <label for="name" class="block text-sm font-medium text-gray-900">Nome</label>
-                <input type="text" id="name"
+                <input type="text" id="name" name="name"
                     class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                     placeholder="Joãozinho" required>
             </div>
             <div class="mb-4">
+                <!-- Input de email de usuario, name = email -->
                 <label for="email" class="block text-sm font-medium text-gray-900">Email</label>
                 <input type="email" id="email"
                     class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                     placeholder="you@example.com" required>
             </div>
             <div class="mb-6">
+                <!-- Input de senha de usuario, name = password -->
                 <label for="password" class="block text-sm font-medium text-gray-900">Senha</label>
-                <input type="password" id="password"
+                <input type="password" id="password" name="password"
                     class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    placeholder="********" required>
+                    placeholder="123joao" required>
             </div>
             <div class="text-center">
-                <a href="#" class="font-bold text-indigo-600 transition duration-300 hover:text-indigo-800">Fazer login</a>
+                <!-- Link para pagina de Login -->
+                <a href="login.php" class="font-bold text-indigo-600 transition duration-300 hover:text-indigo-800">Fazer login</a>
                 <p class="my-5">ou</p>
             </div>
             <div class="mt-6">
+                <!-- Botao para envio do Formulário -->
                 <button type="submit"
                     class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                     Criar conta
